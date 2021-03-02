f4ea8a730063f03fd24628819e7019e71d3dbe62
vlavirus <androando777@gmail.com>
2021-03-02 19:20:15 +0200

    feat(core) create app, add layout, fonts and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteCreatorComponent } from './shared/components/note-creator/note-creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteCreatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/note-creator/note-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-creator',
+  templateUrl: './note-creator.component.html',
+  styleUrls: ['./note-creator.component.scss']
+})
+export class NoteCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
